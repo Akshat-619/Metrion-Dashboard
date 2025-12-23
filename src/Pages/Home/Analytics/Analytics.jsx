6eafee80aa2580a826da2b8c2791d8680513d370
Akshat Sharma <Akshat.Sharma@ad.mastersofterp.com>
2025-12-23 19:46:31 +0530

    designed Home/Analytics

--- src/Pages/Home/Analytics/Analytics.jsx
+++ src/Pages/Home/Analytics/Analytics.jsx
@@ -1,75 +1,83 @@
 import React from 'react'
 import "./Analytics.css"
 import profitIcon from "../../../assets/profitIcon.png";
-import lossIcon from "../../../assets/lossIcon.png"
-import cardDivider from "../../../assets/cardDivider.png"
+import lossIcon from "../../../assets/lossIcon.png";
 
 const Analytics = () => {
   return (
-    <div className='col-12'>
-      <div className=" d-flex mt-5 counts">
-        <div className='cards m-0 d-flex'>
-          {/* Card-1 */}
-          <div className="card-1 ">
-            <div>
-              <h6>Available to withdraw<sup id='neutral-sup'><img  src={profitIcon} alt="" />10.0%</sup></h6>
-            </div>
-            <div className="d-flex mt-3 withdrawal-amt">
-              <h1>$1,567.99</h1>
-              <p>Wed, Jul 20</p>
-            </div>
-          </div>
-   <img className='cardDivider' src={cardDivider} />
+    <div className="col-12 px-4">
+      <div className="counts  mx-auto mt-5">
 
-{/* Card-2 */}
-<div className="card-2">
-            <div>
-              <h6>Today Revenue<sup id='loss-sup'><img src={lossIcon} alt="" />3.0%</sup></h6>
-            </div>
-            <div className="d-flex mt-3 withdrawal-amt">
-              <h1>$2,868.99</h1>
-              <p>143 Orders</p>
+        <div className="row g-4 align-items-center">
+          
+          {/* Card 1 */}
+          <div className="col-3">
+            <div className="card-item card-1">
+              <h6>
+                Available to withdraw
+                <sup id="neutral-sup">
+                  <img src={profitIcon} alt="" /> 10.0%
+                </sup>
+              </h6>
+              <div className="d-flex mt-3 withdrawal-amt">
+                <h1>$1,567.99</h1>
+                <p>Wed, Jul 20</p>
+              </div>
             </div>
           </div>
-<img className='cardDivider' src={cardDivider} />
 
-{/* Card-3 */}
-<div className="card-3">
-   <div>
-              <h6>Today Sessions<sup id='profit-sup'><img src={profitIcon} alt="" />3.2%</sup></h6>
-            </div>
-            <div className="d-flex mt-3 withdrawal-amt">
-              <h1>156k</h1>
-              <p>32k Visitors</p>
+          {/* Card 2 */}
+          <div className="col-3">
+            <div className="card-item card-2">
+              <h6>
+                Today Revenue
+                <sup id="loss-sup">
+                  <img src={lossIcon} alt="" /> 3.0%
+                </sup>
+              </h6>
+              <div className="d-flex mt-3 withdrawal-amt">
+                <h1>$2,868.99</h1>
+                <p>143 Orders</p>
+              </div>
             </div>
           </div>
-</div>
-<img className='cardDivider' src={cardDivider} />
 
-{/* Card-4 */}
-
-<div className="card-4">
-  <div>
-              <h6>Subscribers<sup id='profit-sup'><img src={profitIcon} alt="" />8.3%</sup></h6>
+          {/* Card 3 */}
+          <div className="col-3">
+            <div className="card-item card-3">
+              <h6>
+                Today Sessions
+                <sup id="profit-sup">
+                  <img src={profitIcon} alt="" /> 3.2%
+                </sup>
+              </h6>
+              <div className="d-flex mt-3 withdrawal-amt">
+                <h1>156k</h1>
+                <p>32k Visitors</p>
+              </div>
             </div>
-            <div className="d-flex mt-3 withdrawal-amt">
-              <h1>3,422</h1>
-              <p>$32.48 Average Order</p>
+          </div>
+
+          {/* Card 4 */}
+          <div className="col-3">
+            <div className="card-item card-4">
+              <h6>
+                Subscribers
+                <sup id="profit-sup">
+                  <img src={profitIcon} alt="" /> 8.3%
+                </sup>
+              </h6>
+              <div className="d-flex mt-3 withdrawal-amt">
+                <h1>3,422</h1>
+                <p>$32.48 Average Order</p>
+              </div>
             </div>
-            <img className='cardDivider' src={cardDivider} />
-</div>
-
-
-
-
-
-
+          </div>
 
         </div>
       </div>
-    
-
+    </div>
   )
 }
 
-export default Analytics
+export default Analytics;
